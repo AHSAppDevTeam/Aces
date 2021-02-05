0a20f5ee56c6810446b8be2c73d06eb1254827b2
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-02-05 23:31:20 +0000

    move external scripts to modules

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd scripts
-cat marked.min.js config.js auth.js init.js search.js resize.js editor.js preview.js article.js notif.js \
+cat modules/*.js config.js auth.js init.js search.js resize.js editor.js preview.js article.js notif.js \
 > ../dist/editor.min.js
 
 cd ../styles
-cat normalize.css main.css editor.css auth.css \
+cat modules/*.css main.css editor.css auth.css \
 > ../dist/editor.min.css
