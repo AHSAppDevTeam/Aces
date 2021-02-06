43c400fdca570b2a05bc942766191ff44feb1b3c
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-02-06 02:35:26 +0000

    Auto add random version numbers

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+
+# add random version number to links in order to clear cache
+rand=$(date +%s)
+sed -i -e "s/\?v=\w\+/\?v=$rand/g" editor.html
+
+# merge scripts
 cd scripts
 cat modules/*.js config.js auth.js init.js search.js resize.js editor.js preview.js article.js remote.js \
 > ../dist/editor.min.js
 
+# merge styles
 cd ../styles
 cat modules/*.css main.css editor.css auth.css \
 > ../dist/editor.min.css
