3105e7bc3f50af805816854d08c12a95d30dd107
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-03-20 23:07:09 +0000

    basic grid styling [build]

--- src/js/modules/preview.js
+++ src/js/modules/preview.js
@@ -10,7 +10,7 @@ function updatePreview($preview,id,snippet){
 	const $title = $('.title',$preview)
 	$title.innerHTML = snippet.title
 	$title.href = rot13(id)
-	// $('.')
+	$('.featured',$preview).checked = snippet.featured
 	if(snippet.thumbURLs)
 		$preview.style.backgroundImage = `linear-gradient(var(--cover),var(--cover)), url(${snippet.thumbURLs[0]})`
 }
