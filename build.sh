d7affc5a83cae448c0707fe1c35d1e95006fe71a
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-04-24 21:30:00 +0000

    remove comments from dist files

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd src
 html=$(cat index.html)
-js=$(cd js && cat config.js modules/*.js init.js)
+js=$(cd js && cat config.js modules/*.js init.js | gcc -undef -E -P -xc -)
 css=$(cd css && cat *.css)
 cd ..
 printf "$html" "$css" "$js" > dist/index.html
