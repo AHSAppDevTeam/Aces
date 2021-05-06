e60c40cb08e36c266efff5872d3bce9f67b97c69
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-05-06 21:53:18 +0000

    fix search

--- src/js/modules/browser.js
+++ src/js/modules/browser.js
@@ -10,7 +10,7 @@ async function initBrowser(){
 	updateBrowser()
 	$('#search').addEventListener('input',({target:{value:query}})=>{
 		$$('.preview>h4>.title',$('#browser')).forEach($title=>{
-			const $preview = $title.parentElement.parentElement
+			const $preview = $title.parentElement
 			const show = query ? $title.textContent.toLowerCase().includes(query.toLowerCase()) : true
 			show ? $preview.removeAttribute('hidden') : $preview.setAttribute('hidden','')
 		})
