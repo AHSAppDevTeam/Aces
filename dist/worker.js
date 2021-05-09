2364263fd794e9ab8405f1e72a7cd4cffda0c884
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-05-09 16:59:19 +0000

    [untested] replace EventSource with hacked-together fetch polyfill

--- dist/worker.js
+++ dist/worker.js
@@ -1,4 +1,3 @@
-if('EventSource' in self)
 self.addEventListener('fetch', event => event.respondWith(response(event.request)))
 
 self.addEventListener('install', event => event.waitUntil(self.skipWaiting()))
@@ -11,28 +10,43 @@ const response = request => new Promise(async (resolve)=>{
 	console.log(request.url,cachedResponse)
 	if(cachedResponse) return resolve(cachedResponse)
 	cache.put(request,new Response())
-	const source = new EventSource(request.url)
+	const eventStream = await fetch(
+		request.url,
+		{ headers: { Accept: 'text/event-stream' } }
+	)
+	const reader = eventStream.body.getReader()
 	let first = true
-	source.addEventListener('put', async ({data})=>{
-		console.log(data)
-		const payload = JSON.parse(data)
-		let responseObject = payload.data
-		if(first) { first = false } else {
-			const cachedResponse = await cache.match(request)
-			responseObject = await cachedResponse.json()
-			let modifiedPath = payload.path.split('/').filter(x=>x)
-			while(modifiedPath.length>1)
-				responseObject = responseObject[modifiedPath.shift()]
-			responseObject[modifiedPath[0]] = payload.data
-			// const clients = await self.clients.matchAll({type:'window'})
-			// for(const client of clients)
-			// 	client.postMessage({
-			// 		type: 'update',
-			// 		path: request.headers.get('path'),
-			// 	})
+	while (true) {
+		const { done, value } = await reader.read()
+		if(done) break
+		const { event, data } = Object.fromEntries(
+			new TextDecoder().decode(value)
+			.split('\n').filter(Boolean).map(
+				x => x.split(/:\s?(.+)/).filter(Boolean)
+			)
+		)
+		switch ( event ){
+			case 'put':
+				const payload = JSON.parse(data)
+				let responseObject = payload.data
+				if (first) { first = false } else {
+					const cachedResponse = await cache.match(request)
+					responseObject = await cachedResponse.json()
+					let modifiedPath = payload.path.split('/').filter(x=>x)
+					while(modifiedPath.length>1)
+						responseObject = responseObject[modifiedPath.shift()]
+					responseObject[modifiedPath[0]] = payload.data
+					const clients = await self.clients.matchAll({type:'window'})
+					for(const client of clients)
+						client.postMessage({
+							type: 'update',
+							path: request.headers.get('path'),
+						})
+				}
+				const response = new Response(JSON.stringify(responseObject))
+				cache.put(request,response.clone())
+				resolve(response.clone())
+			// case 'patch':
 		}
-		const response = new Response(JSON.stringify(responseObject))
-		cache.put(request,response.clone())
-		resolve(response.clone())
-	})
+	}
 })
